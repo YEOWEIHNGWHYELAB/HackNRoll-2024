e890fa599b96cc6042374a4421f7a210bf417d75
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2024-01-20 23:05:04 +0800

    Add new breachedNodes table to pgsql

--- backend/dbmanager/initpgdb.sql
+++ backend/dbmanager/initpgdb.sql
@@ -12,3 +12,9 @@ CREATE TABLE IF NOT EXISTS PasswordHistory (
     PRIMARY KEY (element_id, time_changed, password),
     FOREIGN KEY (username) REFERENCES Users(username)
 );
+
+CREATE TABLE IF NOT EXISTS BreachedNodes (
+    element_id VARCHAR(255) PRIMARY KEY,
+    breached BOOLEAN NOT NULL,
+    active BOOLEAN DEFAULT true NOT NULL
+);
